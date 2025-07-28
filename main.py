dac8af7d2eaaaef0fba5a1fbf3fc28b2c6ec5070
Yassien Tawfik <yassien.m.m.tawfik@gmail.com>
2025-07-28 19:17:31 +0300

    Add class distribution plotting to dataset setup in `main.py`

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
-from src.data_setup import download_dataset
+from pathlib import Path
+from src.data_setup import download_dataset, save_class_distribution
 
 
 def main():
     # Step 1: Prepare data
     print("🔽 Downloading GTSRB from KaggleHub...")
     download_dataset()
+
+    print("🔽 Plotting class distribution...")
+    save_class_distribution()
 
 
 if __name__ == "__main__":
